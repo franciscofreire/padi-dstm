1b775623e7f840477f6db0c94e7598f97e698860
Rita Policarpo <ritinha87@gmail.com>
2014-03-29 18:51:08 +0000

    Commands AccessPadInt and CreatePadInt, private class DataServerInfo and some data structures at the MasterServer.

---
+++ padi-dstm/DataServer/DataServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace PADI_DSTM {
+    namespace DataServer {
+        class DataServer {
+            // Hashtable regarding the PadInts stored
+            private Hashtable padInts = new Hashtable();
+
+            public IPadInt CreatePadInt(int uid) {
+                if (!padInts.Contains(uid)) {
+                    PadInt obj = new PadInt();
+                    padInts.Add(uid, obj);
+                    return obj;
+                } else {
+                    return null;
+                }
+            }
+
+            public IPadInt AccessPadInt(int uid) {
+                if (padInts.Contains(uid)) {
+                    return padInts[uid];
+                } else {
+                    return null;
+                }
+            }
+        }
+
+        class Program {
+            static void Main(string[] args) {
+            
+            // TODO: Send info to Master
+            }
+        }
+    }
+}
